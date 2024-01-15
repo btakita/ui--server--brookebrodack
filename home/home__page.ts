08cde743e017388ae18a2e4c039701df21bd8dde
Brian Takita <brian.takita@gmail.com>
2024-01-14 23:37:49 -0500

    update dependencies

--- home/home__page.ts
+++ home/home__page.ts
@@ -3,7 +3,7 @@ import { a_, div_, h1_, li_, ul_ } from 'relementjs/html'
 import type { route_ctx_T } from 'relysjs'
 import { layout_c_ } from '../layout/index.ts'
 export function home__page_<env_T extends relement_env_T = 'server'>({ ctx }:{ ctx: route_ctx_T }) {
-  return (
+	return (
 		layout_c_<env_T>({ ctx },
 			div_({ class: 'home__page' },
 				h1_('Brooke Brodack')),
