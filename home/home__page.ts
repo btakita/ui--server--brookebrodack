25dc27eb91f5e9001f9c96d0886ed07a5e6be03d
Brian Takita <brian.takita@gmail.com>
2024-01-27 19:48:56 -0500

    ./home: home__page_: + social media links:
    	∋ fa_youtube_: https://www.youtube.com/@BrookeABrodack
    	∋ fa_patreon_: https://www.patreon.com/BrookeBrodack
    	∋ fa_instagram_: https://www.instagram.com/brookebrodack
    	∋ fa_linkedin_in_: https://www.linkedin.com/in/brooke-brodack-2223427

--- home/home__page.ts
+++ home/home__page.ts
@@ -1,5 +1,6 @@
+import { fa_instagram_, fa_linkedin_in_, fa_patreon_, fa_youtube_ } from '@btakita/ui--any--brookebrodack/icon'
 import { class_ } from 'ctx-core/html'
-import { raw_ } from 'relementjs'
+import { raw_, type tag_T } from 'relementjs'
 import { a_, div_, h1_, li_, ul_ } from 'relementjs/html'
 import { type request_ctx_T } from 'relysjs/server'
 import { layout_c_ } from '../layout/index.ts'
@@ -32,7 +33,38 @@ export function home__page_({ ctx }:{ ctx:request_ctx_T }) {
 							'inline',
 							'text-center')
 					},
-					raw_('Brookers&hellip;'))))
+					raw_('Brookers&hellip;')))),
+				ul_({
+					class: class_(
+						'fixed',
+						'bottom-2',
+						'z-10',
+						'h-6',
+						'w-screen',
+						'flex',
+						'flex-column',
+						'items-center',
+						'justify-center')
+				}, ([
+					['https://www.youtube.com/@BrookeABrodack', fa_youtube_],
+					['https://www.patreon.com/BrookeBrodack', fa_patreon_],
+					['https://www.instagram.com/brookebrodack', fa_instagram_],
+					['https://www.linkedin.com/in/brooke-brodack-2223427', fa_linkedin_in_],
+				] as social_aa).map(([href, icon_])=>
+					li_({
+						class: class_(
+							'h-6',
+							'w-6',
+							'mx-1')
+					}, [
+						a_({
+							href,
+							target: '_blank'
+						}, icon_({
+							class: class_('dark:fill-gray-100')
+						}))
+					])))
 			]))
 	)
 }
+type social_aa = [href:string, icon_:tag_T][]
