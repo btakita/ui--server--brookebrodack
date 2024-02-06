d34a11691ef0fb8f6ebb4aad0cfa0ed8114d98b1
Brian Takita <brian.takita@gmail.com>
2024-02-06 05:45:51 -0500

    import from 'node:path'
    + ./store: wip
    
    dependencies:
    
    	@btakita/ui--server--blog→@rappstack/ui--server--blog

--- bin/ui--server--brookebrodack__public__ln.js
+++ bin/ui--server--brookebrodack__public__ln.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 import { param_r_ } from 'ctx-core/cli-args'
 import { link, rm } from 'node:fs/promises'
-import { dirname, join } from 'path'
+import { dirname, join } from 'node:path'
 const param_r = param_r_(process.argv.slice(2), {
 	help: '-h, --help',
 	public: '-p, --public'
