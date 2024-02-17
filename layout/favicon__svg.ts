b98d93d0a1b2563ceaaacd73c86b94fe21dda317
Brian Takita <brian.takita@gmail.com>
2024-02-17 10:25:18 -0500

    ./layout:
    	- ./favicon.svg.ts
    	+ favicon__svg_
    	layout__doc_html_: ∋ /favicon.svg

--- layout/favicon__svg.ts
+++ layout/favicon__svg.ts
@@ -1,11 +1,14 @@
 import { heroicons_film_ } from '@btakita/ui--any--brookebrodack/icon'
-const svg = '' + heroicons_film_<'server'>({
-	// language=css
-	css: `
+export function favicon__svg_() {
+	return '' + heroicons_film_<'server'>({
+		width: '24px',
+		height: '24px',
+		// language=css
+		css: `
 svg { stroke: black; }
 @media (prefers-color-scheme: dark) {
 	svg { stroke: white; }
 }
 	`.trim()
-})
-export default svg
+	})
+}
--- layout/layout__doc_html.ts
+++ layout/layout__doc_html.ts
@@ -6,7 +6,6 @@ import { body_, head_, link_, meta_, script_, title_ } from 'relementjs/html'
 import { doc_html_ } from 'relementjs/server'
 import { assets_, assets__new, type assets_T, type request_ctx_T, request_url_ } from 'relysjs/server'
 import brooke_brodack_logo_jpg from '../public/assets/images/brooke-brodack-logo.jpg'
-import favicon_svg from '../public/assets/images/heroicons/film.svg'
 const google_site_verification = import_meta_env_().PUBLIC_GOOGLE_SITE_VERIFICATION
 export function layout__doc_html_({
 	ctx,
@@ -70,7 +69,7 @@ export function layout__doc_html_({
 					rel: 'stylesheet'
 				}),
 				meta_({ name: 'theme-color', content: '' }),
-				link_({ rel: 'icon', type: 'image/svg+xml', href: favicon_svg }),
+				link_({ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }),
 				link_({ rel: 'canonical', href: canonical_url }),
 				link_({ rel: 'sitemap', href: '/sitemap-index.xml' }),
 				google_site_verification
